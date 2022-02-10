ad6614f79f732d95926cab58b739807f9325de2d
Wesley Almeida <obilhao@gmail.com>
2022-02-10 02:37:42 -0300

    adicionando configuracoes

---
+++ user_profile.ps1
@@ -0,0 +1,11 @@
+#Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt Paradox
+#Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
