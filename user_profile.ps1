9a00305fceef1e9302a292ca3404ffde977e39d7
Wesley Almeida <obilhao@gmail.com>
2022-02-14 15:28:01 -0300

    wesley modificou

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,8 +1,14 @@
-#Prompt
+# Prompt
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt Paradox
-#Alias
+
+#Load Function 
+function Get-ScriptDirectory {Split-Path $MyInvocation.ScriptName}
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+#alias
 Set-Alias vim nvim
 Set-Alias ll ls
 Set-Alias g git
