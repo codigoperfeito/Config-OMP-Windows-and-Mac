821b97158f53c96305f25107176f9a6bc7ac2fc5
Wesley Almeida <obilhao@gmail.com>
2022-02-18 11:47:56 -0300

    adicionando configuracoes 3

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,12 +1,26 @@
-# Prompt
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt Paradox
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
-#Load Function 
-function Get-ScriptDirectory {Split-Path $MyInvocation.ScriptName}
-$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) ".\takuya.omp.json"
-oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
 
 #alias
 Set-Alias vim nvim
@@ -15,3 +29,9 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
