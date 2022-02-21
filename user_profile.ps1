7cbf87b26a45346cd6d033194a4b7bf1588eb699
Wesley Almeida <obilhao@gmail.com>
2022-02-21 15:29:28 -0300

    add my theme and modify code to remove bug of lag

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,9 +1,9 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-
+$env:OSH_GIT_ENABLED = $true
 Import-Module posh-git
 Import-Module oh-my-posh
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+$omp_config = Join-Path $PSScriptRoot ".\wesley.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
